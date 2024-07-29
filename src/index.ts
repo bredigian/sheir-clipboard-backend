a2bbdd09f0aa18a20fd3de782f1d097ec5fcf990
Gianluca Bredice <bredicegian@gmail.com>
2024-07-29 19:17:31 -0300

    feature: add "ip" dependency

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import Express from "express"
+import { Server } from "socket.io"
+import { address } from "ip"
 import { config } from "dotenv"
 import cors from "cors"
+import { createServer } from "http"
 
 config()
 
@@ -12,7 +15,29 @@ app.use(Express.json())
 const PORT = process.env.PORT ?? 3000
 
 app.get("/", (_, res) => {
-  res.json({ message: "Express.js + TypeScript Backend" })
+  res.json({ message: "LocalClipboard Websocket", ip: address() })
 })
 
-app.listen(PORT, () => console.log(`Server running at PORT ${PORT}`))
+const socketServer = createServer(app)
+const io = new Server(socketServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+})
+
+io.on("connection", (socket) => {
+  console.log(socket.handshake.query.user + " is connected.")
+
+  socket.on("add-to-clipboard", async (data: { value: string }) =>
+    socket.broadcast.emit("add-to-clipboard", data)
+  )
+
+  socket.on("disconnect", () =>
+    console.log(socket.handshake.query.user + " is disconnected.")
+  )
+})
+
+socketServer.listen(PORT, () =>
+  console.log(`LocalClipboard Websocket at PORT ${PORT}`)
+)
